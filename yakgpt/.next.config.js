9074e4931f577741b3bc0ca82aad1367884dafb1
Karthik Anantharamu <1077970+akarthik10@users.noreply.github.com>
2023-04-12 19:24:15 -0400

    Create .next.config.js

---
+++ yakgpt/.next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  output: 'standalone',
+  staticPageGenerationTimeout: 1000,
+};
+
+module.exports = nextConfig;
