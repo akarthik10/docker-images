f3305a40d44fbf388e94c99b1d7132831b8ed6c0
Karthik Anantharamu <1077970+akarthik10@users.noreply.github.com>
2023-07-09 00:25:47 -0400

    Delete yakgpt directory

--- yakgpt/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  output: 'standalone',
-  staticPageGenerationTimeout: 1000,
-};
-
-module.exports = nextConfig;
